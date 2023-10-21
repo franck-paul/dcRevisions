011593dc3304a6774ff432b0943a5d17af735bba
franck <carnet.franck.paul@gmail.com>
2023-10-21 12:51:31 +0200

    No need to keep old admin URL schema tests

--- src/Backend.php
+++ src/Backend.php
@@ -64,7 +64,7 @@ class Backend extends Process
 
             if (isset($_GET['id']) && (isset($_GET['patch']) || isset($_GET['revpurge']))) {
                 // We have a post or a page ID
-                if ((preg_match('/post.php\?id=\d+(.*)$/', $_SERVER['REQUEST_URI'])) || (preg_match('/index.php\?process=Post\&id=\d+(.*)$/', $_SERVER['REQUEST_URI']))) {
+                if (preg_match('/index.php\?process=Post\&id=\d+(.*)$/', $_SERVER['REQUEST_URI'])) {
                     // It's a post
                     $redirURL = App::backend()->url()->get('admin.post', ['id' => '%s']);
                     if (isset($_GET['patch'])) {
@@ -75,7 +75,7 @@ class Backend extends Process
                         // Purge
                         App::blog()->revisions->purge($_GET['id'], 'post', $redirURL);
                     }
-                } elseif ((preg_match('/plugin.php\?p=pages\&act=page\&id=\d+(.*)$/', $_SERVER['REQUEST_URI'])) || (preg_match('/index.php\?process=Plugin\&p=pages\&act=page\&id=\d+(.*)$/', $_SERVER['REQUEST_URI']))) {
+                } elseif (preg_match('/index.php\?process=Plugin\&p=pages\&act=page\&id=\d+(.*)$/', $_SERVER['REQUEST_URI'])) {
                     // It's a page
                     $redirURL = App::backend()->url()->get('admin.plugin.pages', ['act' => 'page', 'id' => '%s']);
                     if (isset($_GET['patch'])) {
