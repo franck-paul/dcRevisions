cad8f274a6cfec4a9da6f4b67f4ba7390917d8ab
franck <carnet.franck.paul@gmail.com>
2024-09-07 09:18:56 +0200

    New release 5.6

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'dcRevisions',
     'Allows entries versionning',
     'Tomtom, Franck Paul & contributors',
-    '5.5',
+    '5.6',
     [
         'requires'    => [['core', '2.30']],
         'permissions' => 'My',
