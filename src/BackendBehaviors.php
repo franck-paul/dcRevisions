bd3c16ff3c26b83950e5ec9bd48a0929e92836e5
franck <carnet.franck.paul@gmail.com>
2025-01-09 11:53:12 +0100

    PHPDoc

--- src/BackendBehaviors.php
+++ src/BackendBehaviors.php
@@ -34,6 +34,9 @@ use Dotclear\Helper\Html\Html;
 use Dotclear\Plugin\pages\BackendActions as PagesBackendActions;
 use Exception;
 
+/**
+ * @todo switch Helper/Html/Form/...
+ */
 class BackendBehaviors
 {
     /**
