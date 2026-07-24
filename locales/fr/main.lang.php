e568162472b1c16ba7d52d8fa8f4d6330796363c
franck <contact@open-time.net>
2026-07-24 09:06:34 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,8 +12,8 @@ declare(strict_types=1);
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Allows entries\'s versionning'] = 'Permet la gestion des historiques de modification des entrées';
 L10n::$locales['dcRevisions'] = 'Révisions';
+L10n::$locales['Allows entries\'s versionning'] = 'Permet la gestion des historiques de modification des entrées';
 L10n::$locales['Revisions'] = 'Révisions';
 L10n::$locales['Enable entries\' versionning on this blog'] = 'Active la gestion de l\'historique de modification des entrées sur ce blog';
 L10n::$locales['Revisions:'] = 'Historique des modifications :';
@@ -25,6 +25,7 @@ L10n::$locales['Rev.'] = 'Modif.';
 L10n::$locales['Content identical'] = 'Contenu identique';
 L10n::$locales['CAUTION: This operation will replace all the content by the previous one. Are you sure to want apply this patch on this page?'] = 'Attention : Cette opération va remplacer le contenu actuel par le précédent. Êtes-vous sûr·e de vouloir appliquer cette transformation sur cette page ?';
 L10n::$locales['CAUTION: This operation will delete all the revisions. Are you sure to want to do this?'] = 'Attention : Cette opération va supprimer toutes les révisions. Êtes-vous sûr·e de vouloir les supprimer ?';
+L10n::$locales['Revisions not initialized'] = 'Révisions non initialisées';
 L10n::$locales['All revisions have been deleted.'] = 'Toutes les révisions ont été supprimées.';
 L10n::$locales['Pages'] = 'Pages';
 L10n::$locales['Entries'] = 'Billets';
@@ -34,7 +35,7 @@ L10n::$locales['No revision ID'] = 'Pas d\'identifiant de révision';
 L10n::$locales['You are not allowed to delete revisions of this entry'] = 'Vous n\'êtes pas autorisé·e à supprimer cette révision de ce billet';
 L10n::$locales['You are not allowed to patch this entry with this revision'] = 'Vous n\'êtes pas autorisé·e modifier ce billet avec cette révision';
 L10n::$locales['Id'] = 'Modification';
-L10n::$locales['Author'] = 'Auteur·rice';
+L10n::$locales['Author'] = 'Autrice ou auteur';
 L10n::$locales['Date'] = 'Date';
 L10n::$locales['Status'] = 'Statut';
 L10n::$locales['Actions'] = 'Actions';
